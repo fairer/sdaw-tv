54654a462412a2e1d1641fa72d300d1e5506a563
guillaume.algis <guillaume.algis@797b676e-262e-11df-9191-49dca8b91aeb>
2010-03-02 19:35:13 +0000

    Base skel

---
+++ trunk/app/controllers/application.rb
@@ -0,0 +1,15 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '4a1961bbb7fb506da46a4f143d344f04'
+  
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
+end
