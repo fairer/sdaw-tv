fbc1c80fb3d81e875fdd79b1166ecfc46bf56b6b
guillaume.algis <guillaume.algis@797b676e-262e-11df-9191-49dca8b91aeb>
2010-04-09 15:45:48 +0000

    Added my lastest files (guillaume). In theory, fully functionnal project.

--- public/javascripts/player.js
+++ public/javascripts/player.js
@@ -1,51 +1,54 @@
 document.observe("dom:loaded", function () {
 
-    // Get the video player
-    function getPlayer()
-    {
-        var movieName = "player";
-        if (window.document[movieName])
-        {
-            return window.document[movieName];
-        }
-        else if (navigator.appName.indexOf("Microsoft Internet")==-1)
-        {
-            if (document.embeds && document.embeds[movieName])
-                return document.embeds[movieName];
-            else
-                return null;
-
-        }
-        else // if (navigator.appName.indexOf("Microsoft Internet")!=-1)
-        {
-            return document.getElementById(movieName);
-        }
-    }
-
-    // Use this to change curently played video
     function changeVideo(name)
     {
-        var flashMovie = getPlayer();
-        flashMovie.changeSource("../videos/" + name);
-        $('txt').innerHTML = $('txt').innerHTML + "<br/>video played";
+        $('player').changeSource('/videos/' + name)
     }
 
     // Testing purpose. #TO DO : REMOVE
     document.getElementById("button").onclick = function () {
-        changeVideo('barsandtone.flv');
+        changeVideo('numanuma.flv');
     };
 
     //getPlayer().onVideoEnds = function () {
       //  alert('lol');
     //};
 
-    // When finishing loading page
-    function playFirstVid () {
-        changeVideo('gunther.flv');
-    }
-
     document.onmousemove = function () {
-        playFirstVid();
+        changeVideo('gunther.flv');
         document.onmousemove = null;
     };
+
+    $('play').onclick = function () {
+        $('player').play();
+    }
+
+    $('pause').onclick = function () {
+        $('player').pause();
+    }
+
+
+    $('fs').onclick = function () {
+        $('player').fullscreen();
+    }
+
+
+    function get_urls() {
+        new Ajax.Request('videos/to_xml/', {
+            onSuccess: function(response) {
+                alert(response);
+            }
+        });
+    }
+
+    get_urls();
 });
+
+
+    function onVideoEnds() {
+        alert('end');
+    }
+
+    function onPaused() {
+        alert('paused');
+    }
