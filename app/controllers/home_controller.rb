a3185e194f69be744dbe84e30ed0ba891938beb4
leqksi <leqksi@797b676e-262e-11df-9191-49dca8b91aeb>
2010-05-06 10:46:27 +0000

    Program display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,8 +21,40 @@ class HomeController < ApplicationController
 		@moment = session['moment']
 		@period = session['period']
 		@day = session['day']
-		session['temp'] = 1
-		session['collection'] = [{:name => "kikoo"}, {:name => "lol"}]
+		temp = 0
+		@collection = []
+		split = []
+		temp_collection = Planning.find :all
+		temp_collection.each do |t|
+			if (@period == "AM")
+				@start_time_hour = @moment[0].to_int - 48
+			else
+				@start_time_hour = @moment[0].to_int - 36
+			end
+			if ((t.start_date.strftime("%A") == @day) and (t.start_date.strftime("%H") >= @start_time_hour.to_s) and (t.start_date.strftime("%H") < (@start_time_hour + 4).to_s))
+				split = t.video.split("_")
+				id_video = split[0]
+				num_saison = split[1]
+				num_eps = split[2]
+				if ((num_saison == "0") and (num_eps == "0"))
+					aux = Video.find(:all, :conditions => {:id => id_video})
+					if (aux != [])
+						@collection[temp] = {:hour => t.start_date.hour.to_s + ":" + t.start_date.min.to_s + " ", :name => aux[0].name, :infos => aux[0].genre}
+					else
+						@collection[temp] = {:hour => "", :name => "No such video.", :infos => ""}
+					end
+				else
+					eps = Episode.find(:all, :conditions => {:serie => id_video, :season => num_saison, :episode_number => num_eps})
+					if (eps != [])
+						@collection[temp] = {:hour => t.start_date.hour.to_s + ":" + t.start_date.min.to_s + " ", :name => aux[0].name + " ", :infos => eps[0].season.to_s + " " + eps[0].episode_number.to_s}
+					else
+						@collection[temp] = {:hour => "", :name => "No such eps.", :infos => ""}
+					end
+				end
+				temp += 1
+			end
+		end
+		#@collection = @collection.sort
 	end
 
 end
